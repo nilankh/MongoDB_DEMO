c7dba3dca90bcfed33128a300a5212d99115e414
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-08 22:23:27 +0530

    Learning about mongodb

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose
+  .connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.error('Could not connect to MongoDB...', err))
+
+  const courseSchema = new mongoose.Schema({
+      name: String,
+      author: String,
+      tags: [String],
+      date: {type: Data, default: Date.now},
+      isPublished: boolean
+  })
