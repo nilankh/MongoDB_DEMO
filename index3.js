6d342d3fd7f053e73329ed327f47ecc48105efd3
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-09 10:14:40 +0530

    Logical query operator

--- index3.js
+++ index3.js
@@ -34,7 +34,6 @@ async function createCourse() {
 
 // How to reterive document from mongoDB Database
 async function getCourses() {
-  // Comparision query operator
   //   eq(equal)
   // ne(not equal)
   // gt(greater than)
@@ -44,19 +43,12 @@ async function getCourses() {
   // in
   // nin(not in)
 
-  //   Logical Query Operator
-  // or
-  // and
   const courses = await Course.find()
-  const courses = await Course
-    //   We want courses that is published by Nilank or the courses that are published
-    .find()
-    .or([{ author: 'Nilank Nikhil' }, { isPublished: true }])
-    .and([])
-    // .find({ author: 'Nilank Nikhil', isPublished: true })
-    .limit(10)
-    .sort({ name: 1 }) //ascending order if you want descending then -1.
-    .select({ name: 1, tags: 1 })
+  //   const courses = await Course
+  //     .find({author: 'Nilank Nikhil', isPublished: true})
+  //     .limit(10)
+  //     .sort({name: 1}) //ascending order if you want descending then -1.
+  //     .select({name: 1, tags: 1})
 
   //.find({ price: 10 }) //this will return only the course which has price of 10
   // .find({ price: { $gt: 10 } })
