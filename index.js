e56722db199e692208be101600b856af84e0f856
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-09 09:53:38 +0530

    querying documents'

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const courseSchema = new mongoose.Schema({
 })
 
 //   Now we have schema we need to compile into model, which gives us class here class is 'Course', next we can create object based on that class, and this object maps with document in MongoDB database.
+const Course = mongoose.model('Course', courseSchema) //Course class(pascal naming convention for classes)
 async function createCourse() {
-  const Course = mongoose.model('Course', courseSchema) //Course class(pascal naming convention for classes)
   // Now we are creating object based on above class.
   const course = new Course({
     name: 'Angular Course',
@@ -27,8 +27,19 @@ async function createCourse() {
 
   // Classes, objects
   // Human, Nilank(Nilank is an instance of Human)Like here Classe is Course and instance is Node.js course & save this in db.
-//   course.save() it will take some time to save in db, result will be ready in future.
+  //   course.save() it will take some time to save in db, result will be ready in future.
   const result = await course.save()
   console.log(result)
 }
-createCourse();
+
+// How to reterive document from mongoDB Database
+async function getCourses() {
+  const courses = await Course.find()
+//   const courses = await Course
+//     .find({author: 'Nilank Nikhil', isPublished: true})
+//     .limit(10)
+//     .sort({name: 1}) //ascending order if you want descending then -1.
+//     .select({name: 1, tags: 1})
+    console.log(courses)
+}
+getCourses()
