08155892bd6f84f04c7160f31b20dcea4baec7f5
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-13 10:17:11 +0530

    Embedding

--- embedding.js
+++ embedding.js
@@ -1,44 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose
+  .connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+  .catch((err) => console.error('Could not connect to MongoDB...', err))
 
 const authorSchema = new mongoose.Schema({
   name: String,
   bio: String,
-  website: String
-});
+  website: String,
+})
 
-const Author = mongoose.model('Author', authorSchema);
+const Author = mongoose.model('Author', authorSchema)
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String,
-  author: authorSchema
-}));
+const Course = mongoose.model(
+  'Course',
+  new mongoose.Schema({
+    name: String,
+    author: {
+      type: authorSchema,
+      required: true,
+    },
+  })
+)
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
-    author
-  }); 
-  
-  const result = await course.save();
-  console.log(result);
+    name,
+    author,
+  })
+
+  const result = await course.save()
+  console.log(result)
 }
 
-async function listCourses() { 
-  const courses = await Course.find();
-  console.log(courses);
+async function listCourses() {
+  const courses = await Course.find()
+  console.log(courses)
 }
 
-async function updateAuthor(courseId){
+async function updateAuthor(courseId) {
   // const course = await Course.findById(courseId);
-  const course = await Course.update({_id: courseId}, {
-    $set:{
-      'author.name' : 'Harshita Annadanapu'
+  const course = await Course.update(
+    { _id: courseId },
+    {
+      // $set:{
+      //   'author.name' : 'Harshita Annadanapu'
+      // }
+      $unset: {
+        'author.name': '',
+      },
     }
-  });
+  )
   // course.author.name = 'Neel Punj';
   // course.save()
 }
